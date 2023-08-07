f66ce45affcb8fab3228c41e411507459532958a
Tasreen Mowreen <tmowreen97@gmail.com>
2023-08-07 17:24:11 -0400

    added contactpage component

---
+++ components/ContactPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function ContactPage(){
+  return(
+    <div className="w-screen h-full bg-primary">
+      <div className="p-20">
+        <h1 className="md:text-[1100%] text-5xl font-thin text-primary_text text-left tracking-wide">Contact</h1>
+      </div>
+    </div>
+  )
+}
+
+export default ContactPage;
