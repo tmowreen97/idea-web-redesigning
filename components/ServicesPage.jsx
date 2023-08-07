a2dd08201f8688b586b0941e3edb2a38ccc3a7ec
Tasreen Mowreen <tmowreen97@gmail.com>
2023-08-07 17:26:27 -0400

    added services component

---
+++ components/ServicesPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function ServicesPage(){
+  return(
+    <div className="w-screen h-full bg-primary">
+      <div className="p-20">
+        <h1 className="md:text-[1100%] text-5xl font-thin text-primary_text text-left tracking-wide">Services</h1>
+      </div>
+
+    </div>
+    
+  )
+}
+
+export default ServicesPage;
