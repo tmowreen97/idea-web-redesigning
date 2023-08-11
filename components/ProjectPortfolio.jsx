8b6a2062d4411e6852a8437397bb2f664a1e49c8
Tasreen Mowreen <tmowreen97@gmail.com>
2023-08-11 12:29:56 -0400

    project portfolio navigation working!

--- components/ProjectPortfolio.jsx
+++ components/ProjectPortfolio.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
+import { useRouter } from 'next/router';
+import Link from "next/link";
 import Image from "next/image";
 import PortfolioList from "./PortfolioList";
 import {BsChevronCompactDown} from 'react-icons/bs';
 
 function ProjectPortfolio({ projects }){
   const [category, setCategory] = useState('All')
+  const router = useRouter();
   
   // if id is odd, open the description div to the right
   //  if id is even, open the description div to the left
@@ -22,35 +25,35 @@ function ProjectPortfolio({ projects }){
         
         <div className="grid grid-cols-5">
           
-            <button id="category-button-active" className="category-button bg-[#C0B4D3]">
-            All
+            <button id={router.asPath == '/portfolio' ? "category-button-active" : ''} className="category-button bg-[#C0B4D3]">
+            <Link href={'/portfolio'} >All</Link>
           </button>
-          <button className="category-button bg-[#C0B4D3]">
-            Commercial
+          <button id={router.asPath == '/portfolio/commercial' ? "category-button-active" : ''} className="category-button bg-[#C0B4D3]">
+            <Link href={'/portfolio/commercial'}>Commercial</Link>
           </button>
-          <button className="category-button bg-[#C0B4D3]">
-            Cultural
+          <button id={router.asPath == '/portfolio/cultural' ? "category-button-active" : ''} className="category-button bg-[#C0B4D3]">
+            <Link href={'/portfolio/cultural'}>Cultural</Link>
           </button>
-          <button className="category-button bg-[#C0B4D3]">
-            Educational
+          <button id={router.asPath == '/portfolio/educational' ? "category-button-active" : ''} className="category-button bg-[#C0B4D3]">
+            <Link href={'/portfolio/educational'}>Educational</Link>
           </button>
-          <button className="category-button bg-[#C0B4D3]">
-            Healthcare Facilities
+          <button id={router.asPath == '/portfolio/healthcare-facilities' ? "category-button-active" : ''} className="category-button bg-[#C0B4D3]">
+            <Link href={'/portfolio/healthcare-facilities'}>Healthcare Facilities</Link>
           </button>
-          <button className="category-button bg-[#C0B4D3]">
-            Industrial
+          <button id={router.asPath == '/portfolio/industrial' ? "category-button-active" : ''} className="category-button bg-[#C0B4D3]">
+            <Link href={'/portfolio/industrial'}>Industrial</Link>
           </button>
-          <button className="category-button bg-[#C0B4D3]">
-            Institutional
+          <button id={router.asPath == '/portfolio/institutional' ? "category-button-active" : ''} className="category-button bg-[#C0B4D3]">
+            <Link href={'/portfolio/institutional'}>Institutional</Link>
           </button>
-          <button className="category-button bg-[#C0B4D3]">
-            Mixed-Use/High-Rise
+          <button id={router.asPath == '/portfolio/mixed-use-and-high-rise' ? "category-button-active" : ''} className="category-button bg-[#C0B4D3]">
+            <Link href={'/portfolio/mixed-use-and-high-rise'}>Mixed-Use/High-Rise</Link>
           </button>
-          <button className="category-button bg-[#C0B4D3]">
-            Residential
+          <button id={router.asPath == '/portfolio/residential' ? "category-button-active" : ''} className="category-button bg-[#C0B4D3]">
+            <Link href={'/portfolio/residential'}>Residential</Link>
           </button>
-          <button className="category-button bg-[#C0B4D3]">
-            Transportation
+          <button id={router.asPath == '/portfolio/transportation' ? "category-button-active" : ''} className="category-button bg-[#C0B4D3]">
+            <Link href={'/portfolio/transportation'}>Transportation</Link>
           </button>
           </div>
           
@@ -82,7 +85,10 @@ function ProjectPortfolio({ projects }){
         {
           projects.map((proj)=> {
             return(
-              <PortfolioList project={proj}/>
+              <div key={proj.name}>
+                <PortfolioList project={proj}/>
+              </div>
+              
             )
           })
         }
