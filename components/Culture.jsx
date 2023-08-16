1acc4a131630b0118ac83f7ff21a1ef1bb9b666d
Tasreen Mowreen <tmowreen97@gmail.com>
2023-08-16 17:54:33 -0400

    working on mobile nav bar

--- components/Culture.jsx
+++ components/Culture.jsx
@@ -74,7 +74,7 @@ function CulturePage(){
           <div className="culture__child1 md:p-20 p-5 flex bg-secondary/40 rounded-xl">
             <div>
               <h1 className="md:text-[300%] px-5 pt-5 text-primary_text font-light tracking-wide">IDEA HOSTS MIAMI BBQ</h1>
-              <h2 className=" ml-6  text-primary_text text-3xl font-light tracking-wide">June 2023</h2>
+              <h2 className=" ml-6  text-primary_text text-3xl font-light tracking-wide">1</h2>
               <h2 className=" ml-6 mb-3 text-primary_text text-2xl font-light tracking-wide">Nancy Karim</h2>
               <div className="items-center justify-center rounded-xl overflow-hidden">
                 <Image src={'/assets/filler-1.jpg'} width={700} height={300}/>
@@ -90,7 +90,7 @@ function CulturePage(){
           <div className="culture__child2 md:p-20 p-5 mt-5 md:mt-0 flex bg-secondary/40 rounded-xl">
           <div>
               <h1 className="md:text-[300%] px-5 pt-5 text-primary_text font-light tracking-wide">IDEA HOSTS ...</h1>
-              <h2 className=" ml-6 text-primary_text text-3xl font-light tracking-wide">April 2023</h2>
+              <h2 className=" ml-6 text-primary_text text-3xl font-light tracking-wide">2</h2>
               <h2 className=" ml-6 mb-3 text-primary_text text-2xl font-light tracking-wide">Nancy Karim</h2>
               <div className="items-center justify-center rounded-xl overflow-hidden">
                 <Image src={'/assets/filler-2.jpg'} width={500} height={300}/>
@@ -107,7 +107,71 @@ function CulturePage(){
           <div className="culture__child3 md:p-20 p-5 mt-5 md:mt-0 flex bg-secondary/40 rounded-xl">
           <div>
               <h1 className="md:text-[300%] px-5 pt-5 text-primary_text font-light tracking-wide">IDEA HOSTS ... </h1>
-              <h2 className=" ml-6  text-primary_text text-3xl font-light tracking-wide">January 2023</h2>
+              <h2 className=" ml-6  text-primary_text text-3xl font-light tracking-wide">3</h2>
+              <h2 className=" ml-6 mb-3 text-primary_text text-2xl font-light tracking-wide">Nancy Karim</h2>
+              <div className="items-center justify-center rounded-xl overflow-hidden">
+                <Image src={'/assets/filler-3.jpg'} width={500} height={300}/>
+              </div>
+              <div className="pt-10 m-5 text-lg">
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                </p>
+
+              </div>
+            </div>
+          </div>
+          <div className="culture__child4 md:p-20 p-5 mt-5 md:mt-0 flex bg-secondary/40 rounded-xl">
+          <div>
+              <h1 className="md:text-[300%] px-5 pt-5 text-primary_text font-light tracking-wide">IDEA HOSTS ... </h1>
+              <h2 className=" ml-6  text-primary_text text-3xl font-light tracking-wide">4</h2>
+              <h2 className=" ml-6 mb-3 text-primary_text text-2xl font-light tracking-wide">Nancy Karim</h2>
+              <div className="items-center justify-center rounded-xl overflow-hidden">
+                <Image src={'/assets/filler-3.jpg'} width={500} height={300}/>
+              </div>
+              <div className="pt-10 m-5 text-lg">
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                </p>
+
+              </div>
+            </div>
+          </div>
+          <div className="culture__child5 md:p-20 p-5 mt-5 md:mt-0 flex bg-secondary/40 rounded-xl">
+          <div>
+              <h1 className="md:text-[300%] px-5 pt-5 text-primary_text font-light tracking-wide">IDEA HOSTS ... </h1>
+              <h2 className=" ml-6  text-primary_text text-3xl font-light tracking-wide">5</h2>
+              <h2 className=" ml-6 mb-3 text-primary_text text-2xl font-light tracking-wide">Nancy Karim</h2>
+              <div className="items-center justify-center rounded-xl overflow-hidden">
+                <Image src={'/assets/filler-3.jpg'} width={500} height={300}/>
+              </div>
+              <div className="pt-10 m-5 text-lg">
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                </p>
+
+              </div>
+            </div>
+          </div>
+          <div className="culture__child6 md:p-20 p-5 mt-5 md:mt-0 flex bg-secondary/40 rounded-xl">
+          <div>
+              <h1 className="md:text-[300%] px-5 pt-5 text-primary_text font-light tracking-wide">IDEA HOSTS ... </h1>
+              <h2 className=" ml-6  text-primary_text text-3xl font-light tracking-wide">6</h2>
+              <h2 className=" ml-6 mb-3 text-primary_text text-2xl font-light tracking-wide">Nancy Karim</h2>
+              <div className="items-center justify-center rounded-xl overflow-hidden">
+                <Image src={'/assets/filler-3.jpg'} width={500} height={300}/>
+              </div>
+              <div className="pt-10 m-5 text-lg">
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                </p>
+
+              </div>
+            </div>
+          </div>
+          <div className="culture__child7 md:p-20 p-5 mt-5 md:mt-0 flex bg-secondary/40 rounded-xl">
+          <div>
+              <h1 className="md:text-[300%] px-5 pt-5 text-primary_text font-light tracking-wide">IDEA HOSTS ... </h1>
+              <h2 className=" ml-6  text-primary_text text-3xl font-light tracking-wide">7</h2>
               <h2 className=" ml-6 mb-3 text-primary_text text-2xl font-light tracking-wide">Nancy Karim</h2>
               <div className="items-center justify-center rounded-xl overflow-hidden">
                 <Image src={'/assets/filler-3.jpg'} width={500} height={300}/>
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -124,22 +124,34 @@ const Navbar = ({setLightLogo, lightLogo}) => {
         <div
           className={
             nav
-              ? 'sm:hidden absolute top-0 left-0 right-0 bottom-0 flex justify-evenly text-justify items-center w-full h-screen bg-dark_bg ease-in duration-300 tracking-wider'
+              ? 'sm:hidden absolute top-0 left-0 right-0 bottom-0  flex justify-evenly text-justify items-center w-full h-screen bg-dark_bg ease-in duration-300 tracking-wider'
               : 'sm:hidden absolute top-0 left-[-100%] right-0 bottom-0 flex justify-center items-center w-full h-screen bg-dark_bg text-center ease-in duration-300 tracking-wider'
           }
         >
+          
           <ul>
-            <li onClick={handleNav} className='p-4 text-4xl hover:text-gray-500'>
-              <Link href='/'>Home</Link>
+          <div className='flex justify-center'>
+              <Image src={'/assets/clip-1.png'} width={68} height={60} className="m-2"/>
+              <Image src={'/assets/clip-2.png'} width={60} height={60} className="m-2"/>
+              <Image src={'/assets/clip-3.png'} width={60} height={60} className="m-2"/>
+              <Image src={'/assets/clip-4.png'} width={60} height={60} className="m-2"/>
+
+            </div>
+            <ul className="justify-center">
+
+            
+            
+            <li onClick={handleNav} className='p-4 text-4xl hover:text-gray-500 text-center'>
+              <Link href='/' id={router.pathname == "/" ? "active" : ""}>Home</Link>
             </li>
-            <li  className=' hover:text-gray-500 text-4xl p-4 '>
+            <li  className=' hover:text-gray-500 text-4xl p-4 text-center'>
               <div className="dropdown inline-block relative">
                 <button className="inline-flex items-center " id={router.asPath == '/#about' || router.asPath == '/team' ? "active" : ''}>
                   <div className="relative">About</div>
                   <BiChevronDown />
                   {/* <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/> </svg> */}
                 </button>
-                <ul className="dropdown-menu absolute left-5 hidden text-gray-70 text-center z-10">
+                <ul className="dropdown-menu absolute left-0 hidden text-gray-70 text-center z-10">
                   <li onClick={handleNav} className="bg-button_bg hover:bg-accent_2 hover:text-primary_text text-dark_text rounded-xl text-4xl p-2 w-[200px] m-2 ">
                     <Link className="block " href={'/#about'}>About Us</Link>
                   </li>
@@ -149,32 +161,33 @@ const Navbar = ({setLightLogo, lightLogo}) => {
                 </ul>
               </div>
             </li>
-            <li className='p-4 text-4xl hover:text-gray-500'>
+            <li className='p-4 text-4xl hover:text-gray-500 text-center'>
             <div className="dropdown inline-block relative">
-                <button className="inline-flex items-center " id={router.asPath == '/#about' || router.asPath == '/team' ? "active" : ''}>
+                <button className="inline-flex items-center " id={router.asPath == '/#projects' || router.asPath == '/portfolio' ? "active" : ''}>
                   <div className="relative">Projects</div>
                   <BiChevronDown />
                   {/* <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/> </svg> */}
                 </button>
-                <ul className="dropdown-menu absolute left-5 hidden text-gray-70 text-center z-10">
+                <ul className="dropdown-menu absolute left-0 hidden text-gray-70 text-center z-10">
                   <li onClick={handleNav} className="bg-button_bg hover:bg-accent_2 hover:text-primary_text text-dark_text rounded-xl text-4xl p-2 w-[275px] m-2 ">
                     <Link className="block " href={'/#projects'}>Recent Projects</Link>
                   </li>
-                  <li onClick={handleNav} className="bg-button_bg hover:bg-accent_2 right-0 hover:text-primary_text text-dark_text rounded-xl ml-14 text-4xl p-2 w-[200px] m-2">
+                  <li onClick={handleNav} className="bg-button_bg hover:bg-accent_2 right-0 hover:text-primary_text text-dark_text rounded-xl text-4xl p-2 w-[200px] m-2">
                     <Link className="block" href={'/portfolio'}>Portfolio</Link>
                   </li>
                 </ul>
               </div>
             </li>
-            <li onClick={handleNav} className='p-4 text-4xl hover:text-gray-500'>
-              <Link href='/services'>Services</Link>
+            <li onClick={handleNav} className='p-4 text-4xl hover:text-gray-500 text-center'>
+              <Link href='/services' id={router.pathname == "/services" ? "active" : ""}>Services</Link>
             </li>
-            <li onClick={handleNav} className='p-4 text-4xl hover:text-gray-500'>
-              <Link href='/culture'>Culture</Link>
+            <li onClick={handleNav} className='p-4 text-4xl hover:text-gray-500 text-center'>
+              <Link href='/culture' id={router.pathname == "/culture" ? "active" : ""}>Culture</Link>
             </li>
-            <li onClick={handleNav} className='p-4 text-4xl hover:text-gray-500'>
-              <Link href='/contact'>Contact Us</Link>
+            <li onClick={handleNav} className='p-4 text-4xl hover:text-gray-500 text-center'>
+              <Link href='/contact' id={router.pathname == "/contact" ? "active" : ""}>Contact Us</Link>
             </li>
+            </ul>
           </ul>
         </div>
       </div>
