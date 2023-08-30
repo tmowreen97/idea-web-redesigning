a500b30140d01e7c930e21d797ea6dac78f8c5e9
Tasreen Mowreen <tmowreen97@gmail.com>
2023-08-29 23:09:24 -0400

    working on bug

--- components/PortfolioList.jsx
+++ components/PortfolioList.jsx
@@ -38,9 +38,7 @@ function PortfolioList({project}){
     <>
     <div className=" relative p-1" onMouseEnter={()=> setProjHover(true)}
     onMouseLeave={()=> setProjHover(false)}>
-
-    
-      <div className={hide ? "relative rounded-xl overflow-hidden hidden h-full" : "relative rounded-xl overflow-hidden"}>
+      <div className={hide ? "relative rounded-xl overflow-hidden hidden h-full" : "relative rounded-xl overflow-hidden h-full"}>
         <img src={project.image} width={'auto'} height={'auto'} alt={project.name} className={project.custom ? `${project.custom}` : 'w-full h-full rounded-lg'}/>
         {/* <Image src={project.image} width={project.width} height={project.height} alt={'project'}  /> */}
         <div className={projHover ? "absolute top-0 bottom-0 left-0 right-0 bg-[#322d44]/80 text-secondary_text  " : "hidden"}>
