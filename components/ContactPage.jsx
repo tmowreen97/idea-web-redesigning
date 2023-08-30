6e6d913c78b722dedeb0cd8b3310432514c563d7
Tasreen Mowreen <tmowreen97@gmail.com>
2023-08-29 22:26:36 -0400

    updated thank you and contact mobile

--- components/ContactPage.jsx
+++ components/ContactPage.jsx
@@ -10,10 +10,10 @@ function ContactPage(){
       <motion.div 
       initial={{opacity:0}}
       animate={{opacity:1, transition:{duration:2, delay:1} }}
-      className="p-20">
-        <h1 className="md:text-[1100%] text-5xl font-thin text-primary_text text-left tracking-wide">Contact</h1>
+      className="lg:p-20 pt-20 pb-10 px-5 ">
+        <h1 className="md:text-[1100%] text-8xl font-thin text-primary_text text-left tracking-wide">Contact</h1>
       </motion.div>
-      <div className="md:grid md:grid-cols-2 items-center justify-center px-20 ">
+      <div className="md:grid md:grid-cols-2 items-center justify-center lg:px-20 ">
         {/* col/row 1/1 area */}
         <div className=" text-xl flex rounded-xl m-3 pt-2 items-center justify-center ">
           {/* Icons on left handle */}
@@ -21,21 +21,21 @@ function ContactPage(){
             <motion.div
               initial={{opacity:0}}
               animate={{opacity:1, transition:{duration:2, delay:2}}}
-              className="mb-2 "
+              className="lg:mb-2 mb-5"
             >
               <Image src={'/assets/clip-1.png'} width={84} height={82} className=""/>
             </motion.div>
             <motion.div
               initial={{opacity:0}}
               animate={{opacity:1, transition:{duration:2, delay:2.5}}}
-              className="mb-2"
+              className="lg:mb-2 mb-5"
             >
             <Image src={'/assets/clip-2.png'} width={80} height={80}/>              
             </motion.div>
             <motion.div
               initial={{opacity:0}}
               animate={{opacity:1, transition:{duration:2, delay:3}}}
-              className="mb-3"
+              className="lg:mb-3 mb-6"
             >
             <Image src={'/assets/clip-3.png'} width={80} height={80}/>              
             </motion.div>
@@ -52,23 +52,23 @@ function ContactPage(){
           initial={{opacity:0}}
           animate={{opacity:1, transition:{duration:2, delay:1.5}}}
           className="">
-            <div className="pb-[75px]">
-              <div className="pb-[75px]">
-                <div className="flex ">
-                  <h2 className="text-8xl">IDEA</h2>
-                  <h2 className="text-2xl pt-14 ml-3">AEC</h2>
+            <div className="lg:pb-[75px] pb-5">
+              <div className="lg:pb-[75px] pb-5">
+                <div className="flex pb-1">
+                  <h2 className="lg:text-8xl text-6xl">IDEA</h2>
+                  <h2 className="lg:text-2xl text-lg lg:pt-14 pt-8 ml-3">AEC</h2>
                 </div>
-                <h2 className="text-2xl">Inter-Disciplinary Engineering Associates, LLC</h2>
-                <p className="">Architectural Engineering Consultants</p>
+                <h2 className="lg:text-2xl text-lg">Inter-Disciplinary Engineering Associates, LLC</h2>
+                <p className="lg:text-2xl text-lg">Architectural Engineering Consultants</p>
               </div>
-              <p>Monroe, Connecticut | Miami, Florida</p>
+              <p className="text-lg lg:text-lg">Monroe, Connecticut | Miami, Florida</p>
               <p className="text-lg">info@IdeaAEC.com</p>
               <p className="text-lg ">FL: 954-295-2125</p>
               <p className="text-lg ">CT: 203-917-7907</p>
             </div>
             <div>
-              <p className="text-lg">Please feel free to contact us with any questions or business inquiries.</p>
-              <p className="text-lg">You may fill out this form, or contact us by email or phone.</p>
+              <p className="lg:text-lg text-sm">Please feel free to contact us with any questions or business inquiries.</p>
+              <p className="lg:text-lg text-sm">You may fill out this form, or contact us by email or phone.</p>
             </div>
             
           </motion.div>
@@ -78,8 +78,8 @@ function ContactPage(){
         <motion.div 
         initial={{opacity:0}}
         animate={{opacity:1, transition:{duration:2, delay:1.5}}}
-        className="items-center justify-center  flex ">
-        <div className="md:w-[500px] md:h-[400px] bg-secondary rounded-xl ml-20">
+        className="items-center justify-center flex mt-10 lg:mt-0 ">
+        <div className="lg:w-[500px] lg:h-[400px] w-[350px] h-[385px] bg-secondary rounded-xl lg:ml-20">
           <form 
           className="text-center items-center justify-center mt-[3%]" 
           action="https://formsubmit.co/info@ideaaec.com" 
@@ -90,7 +90,7 @@ function ContactPage(){
               <input 
               type="name" 
               name="name" 
-              className="md:w-[400px] h-[40px] rounded-xl text-center p-1" 
+              className="md:w-[400px] w-[300px] h-[40px] rounded-xl text-center p-1" 
               placeholder="Name"
               required/>
             </div>
@@ -99,7 +99,7 @@ function ContactPage(){
               <input 
               type="email" 
               name="email" 
-              className="md:w-[400px] h-[40px] rounded-xl text-center p-1 border-none" 
+              className="md:w-[400px] w-[300px] h-[40px] rounded-xl text-center p-1 border-none" 
               placeholder="Email"
               required/>
             </div>
@@ -108,7 +108,7 @@ function ContactPage(){
               <textarea 
               type="text" 
               name="message" 
-              className="md:w-[400px] md:h-[80px] rounded-xl text-center max-h-[100px] p-1 border-none" 
+              className="md:w-[400px]  w-[300px] md:h-[80px] rounded-xl text-center max-h-[100px] p-1 border-none" 
               placeholder="Message"
               required/>
             </div>
@@ -128,21 +128,21 @@ function ContactPage(){
         initial={{opacity:0}}
         animate={{opacity:1, transition:{duration:2, delay:2.5} }}
         className="flex justify-evenly p-14 pb-24 rounded-xl text-primary_text/90">
-          <div className="bg-tertiary/80 hover:bg-accent_2/80  rounded-full p-7 text-4xl">
+          <div className="bg-tertiary/80 hover:bg-accent_2/80  rounded-full p-7 text-4xl mx-2">
             <Link href="https://www.instagram.com/idea.aec/" legacyBehavior>
                 <a target="_blank" rel="noopener noreferrer">
                   <AiFillInstagram/>
                 </a>
             </Link>
           </div>
-          <div className="bg-tertiary/80 hover:bg-accent_2/80  rounded-full p-7 text-4xl">
+          <div className="bg-tertiary/80 hover:bg-accent_2/80  rounded-full p-7 text-4xl mx-2">
             <Link href="https://www.facebook.com/profile.php?id=100076951517206" legacyBehavior>
                 <a target="_blank" rel="noopener noreferrer">
                   <AiFillFacebook/>
                 </a>
             </Link>
           </div>
-          <div className="bg-tertiary/80 hover:bg-accent_2/80  rounded-full p-7 text-4xl">
+          <div className="bg-tertiary/80 hover:bg-accent_2/80  rounded-full p-7 text-4xl mx-2">
             <Link href="https://www.linkedin.com/company/idea-aec/" legacyBehavior>
                 <a target="_blank" rel="noopener noreferrer">
                   <AiFillLinkedin/>
--- components/ThankYou.jsx
+++ components/ThankYou.jsx
@@ -11,10 +11,10 @@ function ThankYou(){
       <motion.div 
       initial={{opacity:0}}
       animate={{opacity:1, transition:{duration:2, delay:1} }}
-      className="p-20">
-        <h1 className="md:text-[1100%] text-5xl font-thin text-primary_text text-left tracking-wide">Contact</h1>
+      className="lg:p-20 pt-20 pb-10 px-5">
+        <h1 className="md:text-[1100%] text-8xl font-thin text-primary_text text-left tracking-wide">Contact</h1>
       </motion.div>
-      <div className="md:grid md:grid-cols-2 items-center justify-center px-20">
+      <div className="md:grid md:grid-cols-2 items-center justify-center lg:px-20">
         {/* col/row 1/1 area */}
         <div className=" text-xl flex rounded-xl m-3 pt-2 items-center justify-center ">
           {/* Icons on left handle */}
@@ -22,21 +22,21 @@ function ThankYou(){
             <motion.div
               initial={{opacity:0}}
               animate={{opacity:1, transition:{duration:2, delay:2}}}
-              className="mb-2 "
+              className="lg:mb-2 mb-5"
             >
               <Image src={'/assets/clip-1.png'} width={84} height={82} className=""/>
             </motion.div>
             <motion.div
               initial={{opacity:0}}
               animate={{opacity:1, transition:{duration:2, delay:2.5}}}
-              className="mb-2"
+              className="lg:mb-2 mb-5"
             >
             <Image src={'/assets/clip-2.png'} width={80} height={80}/>              
             </motion.div>
             <motion.div
               initial={{opacity:0}}
               animate={{opacity:1, transition:{duration:2, delay:3}}}
-              className="mb-3"
+              className="lg:mb-3 mb-6"
             >
             <Image src={'/assets/clip-3.png'} width={80} height={80}/>              
             </motion.div>
@@ -53,29 +53,29 @@ function ThankYou(){
           initial={{opacity:0}}
           animate={{opacity:1, transition:{duration:2, delay:1.5} }}
           className="">
-            <div className="pb-[75px]">
-              <div className="pb-[75px]">
-                <div className="flex ">
-                  <h2 className="text-8xl">IDEA</h2>
-                  <h2 className="text-2xl pt-14 ml-3">AEC</h2>
+            <div className="lg:pb-[75px] pb-5">
+              <div className="lg:pb-[75px] pb-5">
+                <div className="flex pb-1">
+                  <h2 className="lg:text-8xl text-6xl">IDEA</h2>
+                  <h2 className="lg:text-2xl text-lg lg:pt-14 pt-8 ml-3">AEC</h2>
                 </div>
-                <h2 className="text-2xl">Inter-Disciplinary Engineering Associates, LLC</h2>
-                <p className="">Architectural Engineering Consultants</p>
+                <h2 className="lg:text-2xl text-lg">Inter-Disciplinary Engineering Associates, LLC</h2>
+                <p className="lg:text-2xl text-lg">Architectural Engineering Consultants</p>
               </div>
-              <p>Monroe, Connecticut | Miami, Florida</p>
+              <p className="text-lg">Monroe, Connecticut | Miami, Florida</p>
               <p className="text-lg">info@IdeaAEC.com</p>
               <p className="text-lg ">FL: 954-295-2125</p>
               <p className="text-lg ">CT: 203-917-7907</p>
             </div>
             <div>
-              <p className="text-lg">Please feel free to contact us with any questions or business inquiries.</p>
-              <p className="text-lg">You may fill out this form, or contact us by email or phone.</p>
+              <p className="lg:text-lg text-sm">Please feel free to contact us with any questions or business inquiries.</p>
+              <p className="lg:text-lg text-sm">You may fill out this form, or contact us by email or phone.</p>
             </div>
             
           </motion.div>
           
         </div>
-        <div className="text-center justify-evenly mt-[-25px]">
+        <div className="text-center justify-evenly lg:mt-[-25px] mt-10">
           <motion.div 
           initial={{opacity:0}}
           animate={{opacity:1, transition:{duration:2, delay:1.5} }}
@@ -89,7 +89,7 @@ function ThankYou(){
           className=" text-primary_text mt-10"
           >
             <div className="mr-2">
-              <h1 className="p-2 text-lg">In the meantime, check out what we've been up to:</h1>
+              <h1 className="p-2 lg:text-lg text-base ">In the meantime, check out what we've been up to:</h1>
             </div>
             <div className="flex justify-center items-center gap-10 mt-5">
               <motion.div
@@ -117,21 +117,21 @@ function ThankYou(){
         initial={{opacity:0}}
         animate={{opacity:1, transition:{duration:2, delay:2.5} }}
         className="flex justify-evenly p-14 pb-24 rounded-xl text-primary_text/90">
-          <div className="bg-tertiary/80 hover:bg-accent_2/80  rounded-full p-7 text-4xl">
+          <div className="bg-tertiary/80 hover:bg-accent_2/80  rounded-full p-7 text-4xl mx-2">
             <Link href="https://www.instagram.com/idea.aec/" legacyBehavior>
                 <a target="_blank" rel="noopener noreferrer">
                   <AiFillInstagram/>
                 </a>
             </Link>
           </div>
-          <div className="bg-tertiary/80 hover:bg-accent_2/80  rounded-full p-7 text-4xl">
+          <div className="bg-tertiary/80 hover:bg-accent_2/80  rounded-full p-7 text-4xl mx-2">
             <Link href="https://www.facebook.com/profile.php?id=100076951517206" legacyBehavior>
                 <a target="_blank" rel="noopener noreferrer">
                   <AiFillFacebook/>
                 </a>
             </Link>
           </div>
-          <div className="bg-tertiary/80 hover:bg-accent_2/80  rounded-full p-7 text-4xl">
+          <div className="bg-tertiary/80 hover:bg-accent_2/80  rounded-full p-7 text-4xl mx-2">
             <Link href="https://www.linkedin.com/company/idea-aec/" legacyBehavior>
                 <a target="_blank" rel="noopener noreferrer">
                   <AiFillLinkedin/>
