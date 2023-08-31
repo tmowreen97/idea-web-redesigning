e9db8fad5675c580d8e35667d24cfc452ba72c76
Tasreen Mowreen <tmowreen97@gmail.com>
2023-08-31 12:43:03 -0400

    added drop down feature. if drop down is open, and anywhere on the screen is clicked, it will close the dropdown

--- components/ProjectPortfolio.jsx
+++ components/ProjectPortfolio.jsx
@@ -10,57 +10,29 @@ function ProjectPortfolio({ projects, category }){
   
   const [showDropDown, setShowDropDown] = useState(false)
   const router = useRouter();
-
-  // const useOutsideClick = (callback) => {
-  //   const ref = useRef();
-  
-  //   useEffect(() => {
-  //     const handleClick = (event) => {
-  //       callback();
-  //     };
-  
-  //     document.addEventListener('click', handleClick);
-  
-  //     return () => {
-  //       document.removeEventListener('click', handleClick);
-  //     };
-  //   }, []);
-  
-  //   return ref;
-  // };
-
-  // const handleClick = () => {
-  //   console.log('hi')
-  //   setShowDropDown(true);
-  // };
-  // function handleClickOutside (){
-  //   setShowDropDown(!showDropDown)
-  // }
-  // const ref = useOutsideClick(handleClickOutside);
-  
-  
-    
-  // if id is odd, open the description div to the right
-  //  if id is even, open the description div to the left
-  
+  function handleClick(){
+    if (showDropDown) {
+      setShowDropDown(false)
+    }
+  }
 
 
   return(
-    <div className="w-screen h-full bg-primary">
+    <div className="w-screen h-full bg-primary" onClick={()=> handleClick()}>
       <motion.div 
       initial={{opacity:0}}
       animate={{opacity:1, transition:{delay:1, duration:1}}}
       className="md:px-20 pt-20 pb-10">
         <h1 className="md:text-[1100%] text-8xl font-thin text-primary_text md:text-left text-center tracking-wide">Portfolio</h1>
       </motion.div>
-      <div className="items-center justify-center" >
+      <div className="items-center justify-center" id="outer-container" >
         
         <motion.div 
         initial={{opacity:0}}
         animate={{opacity:1, transition:{delay:1.5, duration:1}}}
         className="flex md:justify-start md:mx-20 justify-center "
         >
-          <div className='relative'>
+          <div className='relative' id='drop_down_parent'>
             {/* <label className="text-2xl text-secondary_text mr-2 ">Select a Category:</label> */}
             <button id='drop_down_button' aria-label='drop_down_button' onClick={()=> {setShowDropDown(!showDropDown)}}  data-dropdown-toggle="dropdown" className="relative text-xl justify-between w-[300px] bg-button_bg/30 hover:bg-secondary/70 focus:ring-4 focus:outline-none focus:ring-secondary_text font-medium rounded-lg  py-2  ml-2 inline-flex items-center text-secondary_text" type="button">
               <p className='mx-2'>{category}</p>
