1493275e45c61ac85a15529d37cc7fc114fb7ef0
Tasreen Mowreen <tmowreen97@gmail.com>
2024-04-08 18:08:27 -0400

    updated services page officially

--- pages/services.js
+++ pages/services.js
@@ -1,12 +1,12 @@
-import ServicesPage from "@/components/ServicesPage";
 import Navbar from "@/components/Navbar";
 import React from "react";
+import ServicesNew from "@/components/ServicesNew";
 
 export default function Services(){
   return (
     <div>
       <Navbar lightLogo={true}/>
-      <ServicesPage/>
+      <ServicesNew/>
     </div>
   )
 }
