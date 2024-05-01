3b804e2e9bb36fe8f12a8e10a008a1209ef30119
Tasreen Mowreen <tmowreen97@gmail.com>
2024-05-01 14:24:47 -0400

    organizing layout

--- components/MSCProject.jsx
+++ components/MSCProject.jsx
@@ -31,31 +31,64 @@ function MSCProject(){
                 </div>
                 </div>
               </div>
+              {/* Heading Text */}
             <div className="flex justify-center px-20 p-10 text-secondary_text tracking-wide xl:max-w-[1600px]">
-            <div className="">
-              <div className="my-4 ">
-                <h2 className="xl:text-5xl text-4xl xl:leading-[60px]">
-                  IDEA AEC has been developing the MEP design for our biggest project yet, MSC Miami Cruise Terminal. Alongside some other talented Architectural and Engineering companies, we've made significant progress.
-                </h2>
-              </div>
-              <div className="my-4 tracking-wider">
-                <h3 className="xl:text-3xl md:text-2xl text-xl xl:leading-[45px]">
-                  A 4-story cruise terminal capable of serving a total of 18,000 cruise ship passengers. The facility consists of 490,000 sf of interior space and 1,200,000 sf garage (6-Levels). There is a boarding concourse approximately 3,000 ft long by 15 ft wide running along the terminal and extending beyond the terminal to the East to accommodate three Berths. The terminal’s programming includes but not limited to: Check-In, Validation, Screening, Waiting Lounges, Office Terrace, Passenger Bridge Concourse, Baggage claim, Baggage, Restrooms, Customs and Border Protection Areas, VIP Lounges, Etc. Sustainable goals include LEED Silver Certification IDEA’s team is also providing MEP-FP Construction Project Management Assistance services to the Design Builder/Construction Team on this opportunity.
-                </h3>
+              <div className="">
+                <div className="my-4 ">
+                  <h2 className="xl:text-5xl text-4xl xl:leading-[60px]">
+                    IDEA AEC has been developing the MEP design for our biggest project yet, MSC Miami Cruise Terminal. Alongside some other talented Architectural and Engineering companies, we've made significant progress.
+                  </h2>
+                </div>
+                <div className="my-4 tracking-wider">
+                  <h3 className="xl:text-3xl md:text-2xl text-xl xl:leading-[45px]">
+                    A 4-story cruise terminal capable of serving a total of 18,000 cruise ship passengers. The facility consists of 490,000 sf of interior space and 1,200,000 sf garage (6-Levels). There is a boarding concourse approximately 3,000 ft long by 15 ft wide running along the terminal and extending beyond the terminal to the East to accommodate three Berths. The terminal’s programming includes but not limited to: Check-In, Validation, Screening, Waiting Lounges, Office Terrace, Passenger Bridge Concourse, Baggage claim, Baggage, Restrooms, Customs and Border Protection Areas, VIP Lounges, Etc. Sustainable goals include LEED Silver Certification IDEA’s team is also providing MEP-FP Construction Project Management Assistance services to the Design Builder/Construction Team on this opportunity.
+                  </h3>
+                </div>
               </div>
             </div>
+            {/* Featuring Container */}
+            <div className="flex justify-left px-20 p-10 xl:max-w-[1600px]">
+              <div>
+                {/* Title */}
+                <div>
+                  <h2>Featuring</h2>
+                </div>
+                {/* Cards */}
+                <div>
+                  <h2>Cards</h2>
+                </div>
+              <div>
 
-          </div>
-          </div>
-          </div>
-          {/* Heading Text */}
-          
-          
-        </div>
+              </div>
+              </div>
+            </div>
+            {/* Services Container */}
+            <div className="flex justify-left px-20 p-10 xl:max-w-[1600px]">
+              <div>
+                <div>
+                  <h2>Services Provided</h2>
+                </div>
+                <div>
+                  <h2>List of Services</h2>
+                </div>
+              </div>
+            </div>
+            {/* Resources */}
+            <div className="flex justify-left px-20 p-10 xl:max-w-[1600px]">
+              <div>
+                <div>
+                  <h2>Resources</h2>
+                </div>
+                <div>
+                  <h2>List of Resources</h2>
+                </div>
+              </div>
+            </div>
+            </div>
+        </div> 
         
       </div>
-      
-      
+      </div>
     </div>
   )
 }
