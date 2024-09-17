254adbd8bf2b09cc7f7fe8f8475172b5432f30ef
Tasreen Mowreen <tmowreen97@gmail.com>
2024-09-17 11:14:55 -0400

    added plausible provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,9 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
 import Head from 'next/head';
-import Footer from '@/components/Footer'
-import { Urbanist } from 'next/font/google'
-import { Analytics } from "@vercel/analytics/react"
+import Footer from '@/components/Footer';
+import { Urbanist } from 'next/font/google';
+import { Analytics } from "@vercel/analytics/react";
+import PlausibleProvider from 'next-plausible'
 
 
 const urbanist = Urbanist({
@@ -10,17 +11,18 @@ const urbanist = Urbanist({
 })
 export default function App({ Component, pageProps }) {
   return (
-    <main 
-    
-    className={urbanist.className}>
-      <Head>
-        <title>IDEA | Inter-Disciplinary Engineering Associates</title>
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-      </Head>
-      <Component {...pageProps} />
-      <Analytics/>
-      <Footer/>
-    </main>
+    <PlausibleProvider>
+      <main      
+      className={urbanist.className}>
+        <Head>
+          <title>IDEA | Inter-Disciplinary Engineering Associates</title>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+        </Head>
+        <Component {...pageProps} />
+        <Analytics/>
+        <Footer/>
+      </main>
+    </PlausibleProvider>
     
   )
 }
